317a87067a619cbe0ff2b03aff0f86b6459b1ded
IntanAprln <infointanapriliana@gmail.com>
2023-04-18 20:27:02 +0800

    mengubah program menu

--- src/menu.java
+++ src/menu.java
@@ -1,38 +1,53 @@
 package TugasPBO;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class menu {
-        private String namaMenu;
-        private int hargaMenu;
-        private String jenisMenu;
-
-        public Menu(String namaMenu, int hargaMenu, String jenisMenu) {
-            this.namaMenu = namaMenu;
-            this.hargaMenu = hargaMenu;
-            this.jenisMenu = jenisMenu;
-        }
-
-        public String getNamaMenu() {
-            return namaMenu;
-        }
-
-        public int getHargaMenu() {
-            return hargaMenu;
-        }
-
-        public String getJenisMenu() {
-            return jenisMenu;
-        }
-
-        public void setNamaMenu(String namaMenu) {
-            this.namaMenu = namaMenu;
-        }
-
-        public void setHargaMenu(int hargaMenu) {
-            this.hargaMenu = hargaMenu;
-        }
-
-        public void setJenisMenu(String jenisMenu) {
-            this.jenisMenu = jenisMenu;
-        }
+    String nama;
+    double harga;
+    int id;
+    public menu (int id, String nama, double harga){
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+    }
+    //kelass makanan
+    public void menuMakanan(String nama, double harga){
+        this.nama = nama;
+        this.harga = harga;
     }
 
+    public void menuMinuman(String nama, double harga){
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    public String toString() {
+        return "Menu : " +
+                nama + " || Rp." + harga;
+    }
+}
