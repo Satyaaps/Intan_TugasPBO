34fe87f082dccd078f4e2c501022a6dca8454ae7
IntanAprln <infointanapriliana@gmail.com>
2023-04-18 20:10:28 +0800

    mengubah login dan main

--- src/Login.java
+++ src/Login.java
@@ -1,56 +1,54 @@
 package TugasPBO;
 
 public class Login {
-    public class Login {
-        private String user;
-        private String pw_user;
-        private String admin;
-        private String pw_admin;
+    private String user;
+    private String password_user;
+    private String admin;
+    private String password_admin;
 
-        public Login() {
-            this.user = "Intan";
-            this.pw_user = "pwuser";
-            this.admin = "Intanoka";
-            this.pw_admin = "dinokuninng";
-        }
+    public Login() {
+        this.user = "Intanuser";
+        this.password_user = "intan12";
+        this.admin = "Intan";
+        this.password_admin = "dinokuning";
+    }
 
-        public Login(String user, String pw_user, String admin, String pw_admin) {
-            this.user = user;
-            this.pw_user = pw_user;
-            this.admin = admin;
-            this.pw_admin = pw_admin;
-        }
+    public Login(String user, String password_user, String admin, String password_admin) {
+        this.user = user;
+        this.password_user = password_user;
+        this.admin = admin;
+        this.password_admin = password_admin;
+    }
 
-        public void setUser(String user) {
-            this.user = user;
-        }
+    public void setUser(String user) {
+        this.user = user;
+    }
 
-        public void setPw_user(String pw_user) {
-            this.pw_user = pw_user;
-        }
+    public void setPassword_user(String password_user) {
+        this.password_user = password_user;
+    }
 
-        public void setAdmin(String admin) {
-            this.admin = admin;
-        }
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
 
-        public void setPw_admin(String pw_admin) {
-            this.pw_admin = pw_admin;
-        }
+    public void setPassword_admin(String password_admin) {
+        this.password_admin = password_admin;
+    }
 
-        public String getUser() {
-            return this.user;
-        }
+    public String getUser() {
+        return this.user;
+    }
 
-        public String getPw_user() {
-            return this.pw_user;
-        }
+    public String getPassword_user() {
+        return this.password_user;
+    }
 
-        public String getAdmin() {
-            return this.admin;
-        }
+    public String getAdmin() {
+        return this.admin;
+    }
 
-        public String getPw_admin() {
-            return this.pw_admin;
-        }
+    public String getPassword_admin() {
+        return this.password_admin;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,27 @@
-package TugasPBO;
-
-    import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-    public class main {
-        public main() {
-        }
-
-        public static void main(String[] args) throws Exception {
-            TugasPBO.Login user_login = new TugasPBO.Login();
-            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Silahkan Login");
-            System.out.print("Masukkan UserName : ");
-            String user = bf.readLine();
-            System.out.print("Masukkan Password : ");
-            String pw = bf.readLine();
-            System.out.println("=============================");
-            if (user.equals(user_login.getUser()) && pw.equals(user_login.getPw_user()) || user.equals(user_login.getAdmin()) && pw.equals(user_login.getPw_admin())) {
-                if (user.equals(user_login.getUser())) {
-                    System.out.println("Selamat datang User");
-                } else if (user.equals(user_login.getAdmin())) {
-                    System.out.println("Selamat datang Admin");
-                }
-            }
-
-        }
+public class main {
+    public main() {
     }
 
+    public static void main(String[] args) throws Exception {
+        TugasPBO.Login user_login = new TugasPBO.Login();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("=============================");
+        System.out.println("Masukkan Username Untuk Login");
+        System.out.print("Masukkan UserName Anda : ");
+        String user = bf.readLine();
+        System.out.print("Masukkan Password Anda : ");
+        String password = bf.readLine();
+        System.out.println("=============================");
+        if (user.equals(user_login.getUser()) && password.equals(user_login.getPassword_user()) || user.equals(user_login.getAdmin()) && password.equals(user_login.getPassword_admin())) {
+            if (user.equals(user_login.getUser())) {
+                System.out.println("Hello, User");
+            } else if (user.equals(user_login.getAdmin())) {
+                System.out.println("Hello, Admin");
+            }
+        }
+
+    }
+}
